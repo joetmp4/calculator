02eb13608f8097e140329bbe47186a0cdb97d74c
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-24 16:19:14 -0400

    nvm lol now backspace works. i never changed the output.textContent...

--- script.js
+++ script.js
@@ -119,10 +119,10 @@ backspace.addEventListener("click", () => { //backspace function; turns output i
     if (tempOutput === "|" || tempOutput === "Error :(") return; //if there is no number return
 
     tempOutput = tempOutput.slice(0,-1); //backspace lol
-
+    
     if (tempOutput === "" || tempOutput === "0" || tempOutput === "-"){ //if remaining char is empty, 0, or neg. num
-        tempOutput = "|"; //reset input if empty
-
+        output.textContent = "|"; //reset input if empty
+        
         if (input.operator === ""){ //check if first number
             input.firstNumber = 0; //reset number
         } else {
@@ -133,6 +133,8 @@ backspace.addEventListener("click", () => { //backspace function; turns output i
         input.decimalPlace = 10;
         return;
     }
+
+    output.textContent = tempOutput;
 
     if(input.operator === ""){//check if first number
         input.firstNumber = Number(tempOutput); //reset number
